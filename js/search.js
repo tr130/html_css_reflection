eb6f8fb4a40bd4b0c5df296296ffe28e84b43c92
Henry Golding <hgolding@protonmail.com>
2021-05-28 16:52:11 +0100

    searchbar resize

--- js/search.js
+++ js/search.js
@@ -41,15 +41,9 @@ searchButton.addEventListener('click', function(e) {
 })
 
 window.addEventListener('resize', function() {
-  if(window.innerWidth >= 1260) {
+  if (window.innerWidth < 990 || window.innerWidth >= 1260) {
   supportButton.style.opacity = '';
   contactButton.style.opacity = '';
-  searchInput.style.opacity = '';
-  supportButton.style.display = '';
-      contactButton.style.display = '';
-      searchForm.style.width = '';
-        searchInput.style.display = '';
-  } else if (window.innerWidth < 1260) {
   searchInput.style.opacity = '';
   supportButton.style.display = '';
       contactButton.style.display = '';
