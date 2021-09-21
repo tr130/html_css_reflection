b24ae5fa75240974fb5ce0d7cd82f8afc8a2c8a6
Henry Golding <hgolding@protonmail.com>
2021-09-21 12:04:32 +0100

    changes to latest posts loop

--- inc/latest.php
+++ inc/latest.php
@@ -7,20 +7,3 @@ $posts = $posts->fetchAll(PDO::FETCH_ASSOC);}
 catch(Exception $e) {
   echo $e->getMessage();
 }
-
-foreach($posts as $post) {
-  $date = DateTime::createFromFormat('Y-m-d', $post['posted_date']);
-  $output = '<div class="col-xs-12 col-md-6 my-col-xl-4" id="latest_' . $post['id'];
-  $output .= '"> <div class="latest_card card latest-' . $post['color'];
-  $output .= '"> <div class="latest_img_container">';
-  $output .= '<a href="' . $post['link'] . '">';
-  $output .= '<img src="img/' . $post['img'] . '" class="latest_img card-img-top" alt="' . $post['alt'] . '"></a>';
-  $output .= '<a href="' . $post['topic_link'] . '" class="latest_topiclink">' . $post['topic'] . '</a></div>';
-  $output .= '<div class="card-body"><a href="' . $post['link'] . '" class="latest_titlelink">' . $post['title'] . '</a>';
-  $output .= '<p class="latest_firstlines">' . $post['first_lines'] . '</p>';
-  $output .= '<a class="btn latest_btn" href="' . $post['link'] . '">Read more</a>';
-  $output .= '<div class="latest_byline"><img class="latest_authorimg" src="img/' . $post['author_img'] . '" alt="author">';
-  $output .= '<div><p class="latest_postedby">' . $post['posted_by'] . '</p>';
-  $output .= '<p class="latest_posteddate">' . $date->format('jS F Y') . '</p></div></div></div></div></div>';
-  echo $output;
-}
